20b52a04a386fe47ecab04c3d468c89aa3383fc2
Jesús Irais González Romero <laygr@outlook.com>
2017-01-02 13:47:24 -0600

    initial

---
+++ build.fsx
@@ -0,0 +1,48 @@
+// include Fake libs
+#r "./packages/FAKE/tools/FakeLib.dll"
+#r @"FakeLib.dll"
+
+open Fake
+open Fake.Azure
+open System
+open System.IO
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+let solutionFile = "transito-cdmx\transito-cdmx.fsproj"
+
+let deployDir = "./"
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDirs [deployDir]
+)
+
+Target "StageWebsiteAssets" (fun _ ->
+    let blacklist =
+        [ "typings"
+          ".fs"
+          ".references"
+          "tsconfig.json" ]
+    let shouldInclude (file:string) =
+        blacklist
+        |> Seq.forall(not << file.Contains)
+    Kudu.stageFolder (Path.GetFullPath @"transito-cdmx\WebHost") shouldInclude)
+
+Target "BuildSolution" (fun _ ->
+    solutionFile
+    |> MSBuildHelper.build (fun defaults ->
+        { defaults with
+            Verbosity = Some Minimal
+            Targets = [ "Build" ]
+            Properties = [ "Configuration", "Release"
+                           "OutputPath", Kudu.deploymentTemp ] })
+    |> ignore)
+
+Target "Deploy" Kudu.kuduSync
+
+"Clean"
+==> "StageWebsiteAssets"
+==> "BuildSolution"
+==> "Deploy"
+
+RunTargetOrDefault "Deploy"
